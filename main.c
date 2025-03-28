22cc306a6604f1a505067c9a6f5528ecf52b81d8
KhuloodGhazi <khulood.gmo@gmail.com>
2025-03-28 20:51:07 +0000

    add _printf.c  _putchar.c  functions.c  main.c  main.h

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point to test _printf function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_printf("Hello, %s!\n", "Khulood");
+	_printf("Character: %c\n", 'A');
+	_printf("Percentage: %%\n");
+	return (0);
+}
+
