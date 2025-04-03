7fddb0f4d6ea0acc2baed9bc9880c3418fc12df0
ab10680 <10680@holbertonstudents.com>
2025-04-03 20:27:33 +0000

    update main.h file

--- main.h
+++ main.h
@@ -4,8 +4,10 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+int _putchar(char c);
 int _printf(const char *format, ...);
-int print_char(va_list args);
-int print_string(va_list args);
+int print_string(const char *s);
+int print_number(int n);
+int handle_specifier(char spec, va_list args, int *printed_chars);
 
 #endif /* MAIN_H */
